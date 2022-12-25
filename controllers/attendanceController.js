bb17d4904fba613a51456e5d49da658110bcd7a0
Will <will413028@gmail.com>
2022-12-25 18:55:14 +0800

    feat: add office hour check for clock out

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -3,46 +3,44 @@ const Attendance = db.Attendance
 const moment = require('moment')
 
 const attendanceController = {
-    postAttendance: (req, res) => {
+    postAttendance: async (req, res) => {
         let create_time;
-
         if(!req.body.create_time) {
             create_time = new Date();
         } else {
             create_time = req.body.create_time
         }
 
-        let attendance_record = Attendance.findOne({ 
+        let attendance_record = await Attendance.findOne({ 
             where: { 
                 user_id: req.body.user_id,
                 attend_date: moment(new Date()).format('YYYY-MM-DD')
             }
         })
 
-        if(attendance_record){
-            Attendance.update({
+        let status = "present";
+
+        if (attendance_record) {
+            diff_hour = moment().diff(attendance_record.toJSON().clock_in_time, 'hour');
+
+            if (diff_hour < 8) {
+                status = "absent";
+            }
+            await attendance_record.update({
                 clock_out_time: create_time,
-                stats: "present"
-            },
-            {
-                where: 
-                {
-                    user_id: req.body.user_id,
-                    attend_date: moment(new Date()).format('YYYY-MM-DD')
-                }
-            }).then(() => {
-                return res.json({ status: 'success', message: 'clock_out successful'})
+                stats: status
             })
+
+            return res.json({ status: 'success', message: 'clock_out successful'})
         } else {
-            Attendance.create({
+            await Attendance.create({
                 user_id: req.body.user_id,
                 clock_in_time: create_time,
                 attend_date: moment(create_time).format('YYYY-MM-DD'),
-                stats: "present"
+                stats: status
             })
-            .then(() => {
-                return res.json({ status: 'success', message: 'clock_in successful'})
-            })
+
+            return res.json({ status: 'success', message: 'clock_in successful'})
         }
     }
 }
