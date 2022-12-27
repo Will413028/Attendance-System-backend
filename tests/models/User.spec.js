9ca6e99d1655cf9f2f79a7f7b986a2b0c8b841ca
Will <will413028@gmail.com>
2022-12-27 16:16:57 +0800

    test: fix association test of user model

--- tests/models/User.spec.js
+++ tests/models/User.spec.js
@@ -5,6 +5,9 @@ const {
   checkPropertyExists
 } = require('sequelize-test-helpers')
 
+const chai = require('chai')
+chai.use(require('sinon-chai'))
+const { expect } = require('chai')
 const UserModel = require('../../models/user')
 
 describe('# User Model', () => {
@@ -20,9 +23,11 @@ describe('# User Model', () => {
 
   context('associations', () => {
     const Attendance = 'Attendance'
-
     before(() => {
       User.associate({ Attendance })
+    })
+    it('defined the hasMany association with Attendance', () => {
+      expect(User.hasMany).to.have.been.calledWith(Attendance)
     })
   })
 })
