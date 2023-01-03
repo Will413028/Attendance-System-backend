a7ca6e184c76f5e47f6dc11522aa3d49cea342f9
Will <will413028@gmail.com>
2023-01-03 13:58:23 +0800

    refactor: add services to reduce duplicate code

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -3,6 +3,7 @@ const Attendance = db.Attendance;
 const Holiday = db.Holiday;
 const moment = require('moment');
 const config = require('../config/config');
+const attendanceServices = require('../services/attendance');
 
 const attendanceController = {
     getAttendance: async (req, res) => {
@@ -45,15 +46,7 @@ const attendanceController = {
     },
 
     createAttendance: async (req, res) => {
-        let workday;
-        let is_after_midnight = moment().isAfter(moment().format('YYYY-MM-DD 00:00:00'));
-        let is_before_workday_change = moment().isBefore(moment().format(`YYYY-MM-DD ${config.ATTENDANCE.TIME_POINT_OF_WORKDAY_CHANGING}`));
-
-        if (is_after_midnight && is_before_workday_change) {
-            workday = moment().add(-1, 'days').format('YYYY-MM-DD');
-        } else {
-            workday = moment().format('YYYY-MM-DD');
-        }
+        let workday = attendanceServices.getWorkday();
 
         let holiday = await Holiday.findOne({
             where: {
@@ -134,16 +127,8 @@ const attendanceController = {
         }
     },
 
-    createAttendanceByQRcode: async (req,res) => {
-        let workday;
-        let is_after_midnight = moment().isAfter(moment().format('YYYY-MM-DD 00:00:00'));
-        let is_before_workday_change = moment().isBefore(moment().format(`YYYY-MM-DD ${config.ATTENDANCE.TIME_POINT_OF_WORKDAY_CHANGING}`));
-
-        if (is_after_midnight && is_before_workday_change) {
-            workday = moment().add(-1, 'days').format('YYYY-MM-DD');
-        } else {
-            workday = moment().format('YYYY-MM-DD');
-        }
+    createAttendanceByQRcode: async (req, res) => {
+        let workday = attendanceServices.getWorkday();
 
         let holiday = await Holiday.findOne({
             where: {
