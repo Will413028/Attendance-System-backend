dffc25c35dffaa5589efb0a54948e493c52957ed
Will <will413028@gmail.com>
2023-01-05 14:59:07 +0800

    fix: modify the db config for heroku

--- models/index.js
+++ models/index.js
@@ -1,18 +1,18 @@
+'use strict';
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-require('dotenv').config()
 
 let sequelize;
-
-sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_ROOT_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  operatorAliases: false
-});
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
@@ -20,7 +20,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
