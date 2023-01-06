4bc238e83cc36ad61eba4c663bbc00f7461eb929
Will <will413028@gmail.com>
2023-01-07 03:15:15 +0800

    test: getWeekday add unit test and refactor getweekday

---
+++ tests/utils/date.spec.js
@@ -0,0 +1,22 @@
+const DateUtil = require('../../utils/date');
+const { expect } = require('chai')
+
+describe('# getWeekday', () => {
+    context('Before time point of weekday changing, weekday should be the previous day', () => {
+        let weekday_from_getWeekday = DateUtil.getWeekday("2022-12-31 04:59:59", "05:00:00");
+
+        it('The weekday of 2022-12-31 04:59:59 should be 2022-12-30', () => {
+
+            expect(weekday_from_getWeekday).equal("2022-12-30");
+        });
+    })
+
+    context('After time point of weekday changing, getweekday can get the current weekday', () => {
+        let weekday = DateUtil.getWeekday("2022-12-31 05:00:00", "05:00:00");
+
+        it('The weekday of 2022-12-31 05:00:00 should be 2022-12-31', () => {
+
+            expect(weekday).equal("2022-12-31");
+        });
+    })
+})
--- utils/date.js
+++ utils/date.js
@@ -2,12 +2,20 @@ const moment = require('moment');
 const config = require('../config/config');
 
 const DateUtil = {
-    getWeekday: () => {
-        let weekday = moment();
-        let is_privously_weekday = weekday.isBefore(moment().format(`YYYY-MM-DD ${config.ATTENDANCE.TIME_POINT_OF_WEEKDAY_CHANGING}`));
+    getWeekday: (time, time_point_changing=config.ATTENDANCE.TIME_POINT_OF_WEEKDAY_CHANGING) => {
+        if (!time) {
+            time = moment();
+        } else {
+            time = moment(time);
+        }
 
+        let is_privously_weekday = time.isBefore(time.format(`YYYY-MM-DD ${time_point_changing}`));
+
+        let weekday;
         if (is_privously_weekday) {
-            weekday = moment().add(-1, 'days').format('YYYY-MM-DD');
+            weekday = time.add(-1, 'days').format('YYYY-MM-DD');
+        } else {
+            weekday = time.format('YYYY-MM-DD');
         }
 
         return weekday;
